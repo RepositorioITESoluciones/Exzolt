0dd041eb64af3345b4bcccdf661aad791d322dee
criistoph <34460628+criistoph@users.noreply.github.com>
2018-05-09 15:44:14 -0500

    Se coloca imagen y se inserta el registro

--- WBExzolt/js/Index.js
+++ WBExzolt/js/Index.js
@@ -4,10 +4,10 @@ var rows;
 var data;
 $(function () {
     initEventos();
-    //initDataTable();
 });
 function initEventos() {
-   
+    var inputFile = document.getElementById('avatar');
+    inputFile.addEventListener('change', mostrarImagen, false);
     $("#divAlias").hide();
     bootsVal();
     $('#radioSi').change(function () {
@@ -28,39 +28,38 @@ function initEventos() {
             bootsVal();
         }
     })
- 
 
     $('#botonReg').click(function () {
         bootsVal();
+        var nombreParametro = "";
+
+        if ($('input#radioSi').is(':checked')) {
+            nombreParametro = $("#alias").val();
+        } else {
+            nombreParametro = $("#nomnbre").val();
+        }
+
         $('#formularioRegistro').data('bootstrapValidator').validate();
         var n = $('#formularioRegistro').data('bootstrapValidator').isValid();
         if (n) {
             $.ajax({
-                async: false,
+                async: true,
                 type: 'POST',
-                url: 'WSExzolt.asmx/ActualizarDatosEmpresariales',
-                data: {
-                    idEmpresa: rows[12],
-                    nombre: $('#nombre').val(),
-                    fechaRegistro: $('#fecha').val(),
-                    idGiro: $('#giro').val(),
-                    ComboTPer: $('#tipoPer').val(),
-                    RazonSocial: $('#razonSocial').val(),
-                    RFC: rfc,
-                    ComboEstado: $('#ComboEstado').val(),
-                    ComboCP: $('#ComboCP').val(),
-                    Calle: $('#calle').val(),
-                    NumExt: $('#numExt').val(),
-                    NumInt: $('#numInt').val()
-                },
-                success: function (data) {
-                    $.smallBox({
-                        title: "Éxito!",
-                        content: "Empresa <b>" + rows[0] + "</b> editada",
-                        color: "#739e73",
-                        timeout: 2000,
-                        icon: "fa fa-thumbs-up swing animated"
+                url: 'WSExzolt.asmx/insertarUsuario',
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                data: '{"nombre": "' + nombreParametro + '"}',
+                success: function (response) {
+                    $.bigBox({
+                        title: "<center><p style='font-size: 20px'>¡Felicidades!</p></center>",
+                        content: "<center><p style='font-size: 20px'>Tu numero de jugador es : <b>" + response.d + "</b> </p></center>",
+                        color: "#739E73",
+                        timeout: "5000",
+                        icon: "fa fa-check"
                     });
+                    $('#botonReg').prop("disabled", false);
+                    $('#nombre').val('');
+                    $('#alias').val('');
                 }
             });
         } else {
@@ -68,216 +67,7 @@ function initEventos() {
             bootsVal();
         }
     });
-
-    $('#btnguardar2').click(function () {
-        var rfc = "";
-        if (banderaRFC == "Fisico") {
-            rfc = $("#rfcFisico").val();
-        } else {
-            rfc = $("#rfcMoral").val();
-        }
-        rfc = rfc.toUpperCase();
-        bootsVal();
-        $('#FormEmpresa').data('bootstrapValidator').validate();
-        var n = $('#FormEmpresa').data('bootstrapValidator').isValid();
-        if (n) {
-            $('#divDatosEmpresariales').show();
-            $('#FormularioDatosEmpresariales').hide();
-
-            $.ajax({
-                async: false,
-                type: 'POST',
-                url: 'WSLinPro.asmx/ActualizarDatosEmpresariales',
-                data: {
-                    idEmpresa: rows[12],
-                    nombre: $('#nombre').val(),
-                    fechaRegistro: $('#fecha').val(),
-                    idGiro: $('#giro').val(),
-                    ComboTPer: $('#tipoPer').val(),
-                    RazonSocial: $('#razonSocial').val(),
-                    RFC: rfc,
-                    ComboEstado: $('#ComboEstado').val(),
-                    ComboCP: $('#ComboCP').val(),
-                    Calle: $('#calle').val(),
-                    NumExt: $('#numExt').val(),
-                    NumInt: $('#numInt').val()
-                },
-                success: function (data) {
-                    $.smallBox({
-                        title: "Éxito!",
-                        content: "Empresa <b>" + rows[0] + "</b> editada",
-                        color: "#739e73",
-                        timeout: 2000,
-                        icon: "fa fa-thumbs-up swing animated"
-                    });
-                }
-            });
-            cargarTabla();
-            $('#FormEmpresa')[0].reset();
-            $('#FormEmpresa').bootstrapValidator('destroy');
-        } else {
-            $('#btnguardar2').prop("disabled", false);
-            bootsVal();
-        }
-    });
-
-   
-
-    
 }
-
-//function initDataTable() {
-//    $.fn.dataTable.ext.errMode = 'none';
-//    var responsiveHelper_datatable_fixed_column = undefined;
-//    var breakpointDefinition = {
-//        tablet: 1024,
-//        phone: 480,
-//        desktop: 1260
-//    };
-//    var datos = [];
-//    $.ajax({
-//        async: false,
-//        type: 'POST',
-//        url: 'WSLinPro.asmx/LlenaTablaDE',
-//        dataType: 'json',
-//        contentType: 'application/json; charset=utf-8',
-//        beforeSend: function () {
-//            $('#loadingMod').modal({
-//                backdrop: 'static',
-//                keyboard: false
-//            });
-//        },
-//        success: function (response) {
-//            $('#loadingMod').modal('hide');
-//            $.each(response, function (row, index) {
-//                $.each(index.ListaRegistros, function (r, arr) {
-//                    var d = new Date(arr.fechaRegistro).forma;
-//                    datos.push([arr.nombre, arr.fechaRegistro.substring(0, 10), arr.DatosFiscales.RFC, arr.DatosFiscales.RazonSocial, arr.DatosFiscales.TipoPersona.TipoPersona, arr.idGiro, arr.DatosFiscales.TipoPersona.IdTipoPersona, arr.DatosFiscales.Estado.idEstado, arr.DatosFiscales.C_CP, arr.DatosFiscales.Calle, arr.DatosFiscales.NumeroExterior, arr.DatosFiscales.NumeroInterior, arr.idEmpresa]);
-//                });
-//            });
-//        }
-
-//    });
-
-//    otable = $('#TablaDatosEmpresariales')
-//        .DataTable({
-
-//            "aLengthMenu": [
-//                [5, 10, 25, 50],
-//                [5, 10, 25, 50]
-//            ],
-//            "sDom": "<'dt-toolbar'<'col-xs-12 col-sm-6 hidden-xs'l><'col-sm-6 col-xs-12 hidden-xs'<'toolbar'>>r>" +
-//            "t" +
-//            "<'dt-toolbar-footer'<'col-sm-6 col-xs-12 hidden-xs'i><'col-xs-12 col-sm-6'p>>",
-//            "oLanguage": {
-//                "sUrl": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
-//            },
-
-//            "autoWidth": true,
-//            "preDrawCallback": function () {
-//                if (!responsiveHelper_datatable_fixed_column) {
-//                    responsiveHelper_datatable_fixed_column = new ResponsiveDatatablesHelper(
-//                        $('#TablaDatosEmpresariales'), breakpointDefinition);
-//                }
-//            },
-//            "rowCallback": function (nRow) {
-//                responsiveHelper_datatable_fixed_column
-//                    .createExpandIcon(nRow);
-//            },
-//            "drawCallback": function (oSettings) {
-//                responsiveHelper_datatable_fixed_column.respond();
-//            },
-//            data: datos,
-//            columns: [{
-//                title: "Nombre"
-//            },
-//            {
-//                title: "Fecha De Registro"
-//            },
-//            {
-//                title: "RFC"
-//            },
-//            {
-//                title: "Razón Social"
-//            },
-//            {
-//                title: "Tipo De Persona"
-//            },
-//            {
-//                title: "idGiro",
-//                visible: false
-//            },
-//            {
-//                title: "Id tipo persona",
-//                visible: false
-//            },
-//            {
-//                title: "idEstado",
-//                visible: false
-//            },
-//            {
-//                title: "cp",
-//                visible: false
-//            },
-//            {
-//                title: "calle",
-//                visible: false
-//            },
-//            {
-//                title: "numExt",
-//                visible: false
-//            },
-//            {
-//                title: "numInt",
-//                visible: false
-//            }
-
-//            ]
-//        });
-
-//    // Evento creado para realizar la búsqueda cuando se presione la tecla ENTER
-//    $("#TablaDatosEmpresariales thead th input[type=text]").on(
-//        'keyup',
-//        function (e) {
-//            otable.column($(this).parent().index() + ':visible').search(
-//                this.value).draw();
-//        });
-
-//    // Método creado para agregar el evento de selección de una fila
-//    $('#TablaDatosEmpresariales tbody').on(
-//        'click',
-//        'tr',
-//        function () {
-//            if ($(this).hasClass('selected')) {
-//                $(this).removeClass('selected');
-//            } else {
-//                $('#TablaDatosEmpresariales').DataTable().$('tr.selected').removeClass(
-//                    'selected');
-//                $(this).addClass('selected');
-//            }
-//        });
-//    // Evento creado para abrir la ventana de editar al dar doble click sobre un
-//    // registro
-//    $('#TablaDatosEmpresariales tbody').on('dblclick', 'tr', function () {
-//        $(this).addClass('selected');
-//        $("#ComboEstado").val(0);
-//        $("#ComboEstado").trigger("change");
-//        $(".estado").removeClass("has-error");
-
-//        $("#ComboCP").val(0);
-//        $("#ComboCP").trigger("change");
-//        $(".codigo").removeClass("has-error");
-
-//        $("#errorCp").hide();
-//        $("#errorEstado").hide();
-//        bootsVal();
-//        editUsuario();
-//        edit = 1;
-
-//    });
-//}
-
-
 
 //Funcion creada para validar campos vacios en formulario
 function bootsVal() {
@@ -306,63 +96,22 @@ function bootsVal() {
         }
     });
 }
+function showOkMessage(titulo, mensaje) {
+    $.smallBox({
+        title: titulo,
+        content: mensaje,
+        color: "#296191",
+        timeout: 4000,
+        icon: "fa fa-thumbs-o-up swing animated"
+    });
+}
 
-
-//function bootsValAlias() {
-
-//    $('#formularioRegistro').bootstrapValidator({
-//        live: 'enabled',
-//        submitButtons: 'button[id="botonReg"]',
-//        message: 'Valor invalido',
-//        fields: {
-//            alias: {
-//                group: '.col-6',
-//                validators: {
-//                    callback: {
-//                        message: 'El alias es obligatorio',
-//                        callback: function (value, validator, $field) {
-//                            //if ($('input#radioSi').is(':checked')) {
-//                                if (value === '0') {
-//                                    return false
-//                                } else {
-//                                    return true
-//                                }
-//                            //}
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    });
-//}
-
-
-////Funcion encargada de refrescar la tabla despues de haber creado, editado o eliminado un registro
-//function cargarTabla() {
-//    var datos = [];
-//    $.ajax({
-//        async: false,
-//        type: 'POST',
-//        url: 'WSLinPro.asmx/LlenaTablaDE',
-//        dataType: 'json',
-//        contentType: 'application/json; charset=utf-8',
-//        beforeSend: function () {
-//            $('#loadingMod').modal({
-//                backdrop: 'static',
-//                keyboard: false
-//            });
-//        },
-//        success: function (response) {
-//            $('#loadingMod').modal('hide');
-//            $.each(response, function (row, index) {
-
-//                $.each(index.ListaRegistros, function (r, arr) {
-//                    datos.push([arr.nombre, arr.fechaRegistro.substring(0, 10), arr.DatosFiscales.RFC, arr.DatosFiscales.RazonSocial, arr.DatosFiscales.TipoPersona.TipoPersona, arr.idGiro, arr.DatosFiscales.TipoPersona.IdTipoPersona, arr.DatosFiscales.Estado.idEstado, arr.DatosFiscales.C_CP, arr.DatosFiscales.Calle, arr.DatosFiscales.NumeroExterior, arr.DatosFiscales.NumeroInterior, arr.idEmpresa]);
-//                });
-//            });
-//        }
-//    });
-//    otable.clear();
-//    otable.rows.add(datos);
-//    otable.draw();
-//}
+function mostrarImagen(event) {
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        var img = document.getElementById('img1');
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(file);
+}
