a64eeb5d7d70f9b08a4f6d27a7e185872a7a2055
criistoph <34460628+criistoph@users.noreply.github.com>
2018-05-15 15:12:54 -0500

    se guarda imagen bd

--- WBExzolt/js/Index.js
+++ WBExzolt/js/Index.js
@@ -2,15 +2,13 @@
 var url;
 var rows;
 var data;
+var imagenBase64;
 var nombreParametro = "";
 $(function () {
     console.log("Hola");
     $("#divAlias").hide();
     initEventos();
-
-    
 });
-
 
 $("#foto").click(() => {
     $('#myModal').modal('show');
@@ -113,13 +111,26 @@ function initEventos() {
 
     $('#botonReg').click(function () {
         bootsVal();
-
         if ($('input#radioSi').is(':checked')) {  
             nombreParametro = $(".alias").val();
         } else {
             nombreParametro = $("#nombre").val();
         }
 
+        var file = document.querySelector('#avatar').files[0];
+        if (file == null) {
+            imagenBase64 = $('#img1').attr('src');
+        } else {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                imagenBase64 = reader.result;
+            };
+            reader.onerror = function (error) {
+                console.log('Error: ', error);
+            };
+
+        }
+        console.log(file);
         $('#formularioRegistro').data('bootstrapValidator').validate();
         var n = $('#formularioRegistro').data('bootstrapValidator').isValid();
 
@@ -155,7 +166,7 @@ function initEventos() {
                             url: 'WSExzolt.asmx/insertarUsuario',
                             dataType: 'json',
                             contentType: 'application/json; charset=utf-8',
-                            data: '{"nombre": "' + nombreParametro + '"}',
+                            data: '{"nombre": "' + nombreParametro + '", "foto": "' + imagenBase64 + '"}',
                             success: function (response) {
                                 $.bigBox({
                                     title: "<center><p style='font-size: 20px'>¡Felicidades!</p></center>",
@@ -226,10 +237,45 @@ function mostrarImagen(event) {
     var reader = new FileReader();
     reader.onload = function (event) {
         var img = document.getElementById('img1');
-        img.src = event.target.result;
+        img.src = event.target.result.toString();
+
+        imagenBase64 = event.target.result.toString();
     }
     try {
         reader.readAsDataURL(file);
     } catch (err) { }
 
+}
+
+function dataURItoBlob(dataURI) {
+    // convert base64 to raw binary data held in a string
+    // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+    var byteString = atob(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+    // write the bytes of the string to an ArrayBuffer
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    // write the ArrayBuffer to a blob, and you're done
+    var bb = new BlobBuilder();
+    bb.append(ab);
+    console.log(bb.getBlob(mimeString));
+    return bb.getBlob(mimeString);
+}
+
+function getBase64(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function () {
+        //imagenBase64 = reader.result;
+    };
+    reader.onerror = function (error) {
+        console.log('Error: ', error);
+    };
 }
